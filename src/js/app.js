3f2973c08817f30766ae249e19f6ca27f10a3806
Atsushi Wada <wada.atsushi@tyrus.co.jp>
2018-09-19 15:41:46 +0900

    フロント側修正

--- src/js/app.js
+++ src/js/app.js
@@ -1,68 +1,39 @@
-App = {
-  web3Provider: null,
-  contracts: {},
+const web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:7545"));
+const address = "0x0a54ca767f704520e20a3f8b137f4ad035651c78"
+const coinbase = web3.eth.accounts[0];
 
-  init: function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
-
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-
-        petsRow.append(petTemplate.html());
-      }
-    });
-
-    return App.initWeb3();
-  },
-
-  initWeb3: function() {
-    /*
-     * Replace me...
-     */
-
-    return App.initContract();
-  },
-
-  initContract: function() {
-    /*
-     * Replace me...
-     */
-
-    return App.bindEvents();
-  },
-
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
-  },
-
-  markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
-  },
-
-  handleAdopt: function(event) {
-    event.preventDefault();
-
-    var petId = parseInt($(event.target).data('id'));
-
-    /*
-     * Replace me...
-     */
-  }
-
-};
-
-$(function() {
-  $(window).load(function() {
-    App.init();
+// 初期化
+function init() {
+  var petsRow = $('#petsRow');
+  var petTemplate = $('#petTemplate');
+  $.getJSON('Petshop.json', function(data) {
+    contract = web3.eth.contract(data.abi).at(address);
+    console.log("contract : ", contract);
+    let name = contract.name.call();
+    tokens = contract.getAllTokens();
+    
+    for(var i = 0; i < tokens.length; i++) {
+      pet = contract.getPet(i);
+      console.log(`Pet : ${pet}`);
+      
+      petTemplate.find('.panel-title').text(pet);
+      //petTemplate.find('img').attr('src', pet);
+      petTemplate.find('.pet-breed').text("柴犬");
+      //petTemplate.find('.pet-age').text(pet);
+      petTemplate.find('.pet-location').text("日本");
+      //petTemplate.find('.btn-adopt').attr('data-id', pet);
+      petsRow.append(petTemplate.html());
+    }
   });
-});
+}
+
+// ペット生成(オーナーのみ)
+function mint() {
+  var owner;
+  console.log("coinbase : ", coinbase);
+  $.getJSON('Petshop.json', function(data) {
+    owner = web3.eth.contract(data.abi).at(coinbase);
+    console.log("owner : ",owner);
+    owner.mint();
+  });
+}
