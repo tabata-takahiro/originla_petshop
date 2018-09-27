a2876e7fb69a4466bfd73d8869ed5f5b57ce0364
Takahiro Tabata <tabata@tyrus.co.jp>
2018-09-27 18:13:57 +0900

    ペットの年齢を日数で表示
    名前も表示

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-const address = "0x013aac85ebc9b2a155de1cbd71c0d5ad773881e3" // コントラクトのアドレス
+const address = "0x8469754b9d0ee46b1b87c1ed6e29cb0af46b5e5c"; // コントラクトのアドレス
 let coinbase = null; // コントラクトを呼び出すアカウントのアドレス
 let web3js;
 let contract;
@@ -25,7 +25,6 @@ function init() {
       }
     });
 
-    let now = new Date()
     contract = web3js.eth.contract(data.abi).at(address);
     contract.getAllTokens.call(function(err, res) {
       tokens = res;
@@ -36,12 +35,13 @@ function init() {
         contract.getPet(token_id, function(error, result) {
           let pet = result;
           let d = new Date(pet[2] * 1000);
+          let age = elapsedDays(d);
           let price = web3js.fromWei(pet[3], 'ether');
           petTemplate.find(`.pet-id`).text(`No : ${Number(token_id) + 1}`);
-          petTemplate.find('.panel-title').text(`${pet[0]}ちゃん`);
+          petTemplate.find('.panel-title').text(`${pet[1]}`);
           petTemplate.find('img').attr('src', `images/${getBreedKey(pet[0])}.jpeg`);
           petTemplate.find('.pet-breed').text(getBreed(getBreedKey(pet[0])));
-          petTemplate.find('.pet-age').text(now.getFullYear() - d.getFullYear());
+          petTemplate.find('.pet-age').text(age);
           petTemplate.find('.pet-location').text(getPrefecture(pet[0]));
           petTemplate.find('.pet-price').text(price);
           petTemplate.find('.btn-adopt').attr('id', token_id);
@@ -126,29 +126,29 @@ const pref={
   '45':'宮崎県',
   '46':'鹿児島県',
   '47':'沖縄県'
-}
+};
 
 const breed = {
   '0':'Scottish Terrier',
   '1':'French Bulldog',
   '2':'Boxer',
   '3':'Golden Retriever'
-}
+};
 /**
-* DNAから出身地を返す
-* @param {number} dna - DNA
-* @returns {string} - 出身地
-*/
+ * DNAから出身地を返す
+ * @param {number} dna - DNA
+ * @returns {string} - 出身地
+ */
 function getPrefecture(dna) {
-  let prefecture_id = String(dna).substring(2, 3)% 47 + 1
-  return pref[String(prefecture_id).padStart(2, '0')]
+  let prefecture_id = String(dna).substring(2, 3)% 47 + 1;
+  return pref[String(prefecture_id).padStart(2, '0')];
 }
 
 /**
-* DNAから犬種キーを返す
-* @param {number} dna - DNA
-* @returns {number} - 犬種キー
-*/
+ * DNAから犬種キーを返す
+ * @param {number} dna - DNA
+ * @returns {number} - 犬種キー
+ */
 function getBreedKey(dna) {
   return String(dna).substring(0, 1) % 4;
 }
@@ -170,4 +170,15 @@ function buyPet(selectObj) {
 function getId(selectObj){
   const petId = selectObj.id; // ペットidを取得
   console.log(petId); //「id01」
+}
+
+/**
+ * 経過日数を返す
+ * @param {Date} date - 日時
+ * @returns {Number} - 日数
+ */
+function elapsedDays(date) {
+  let now = new Date();
+  let diff = now.getTime() - date.getTime();
+  return Math.floor(diff / (1000 * 60 * 60 * 24));
 }
